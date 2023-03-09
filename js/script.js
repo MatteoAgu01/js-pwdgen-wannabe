5ebebb7a98712ec3a42f743dd930878ce5e879c1
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-03-09 01:27:13 +0100

    add ";"

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-
+﻿
 
 /*Chiedi all’utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21*/
 
-const nomeUtente = prompt('Inserisci il tuo nome')
+const nomeUtente = prompt('Inserisci il tuo nome');
 
-const cognomeUtente = prompt('Inserisci il tuo cognome')
+const cognomeUtente = prompt('Inserisci il tuo cognome');
 
-const colorePreferito = prompt('inserisci colore preferito')
+const colorePreferito = prompt('inserisci colore preferito');
 
 if(nomeUtente && cognomeUtente) 
      document.getElementById('psw').innerHTML = `
-     <h1>${nomeUtente}${cognomeUtente}${colorePreferito}21</h1>`
+     <h1>${nomeUtente}${cognomeUtente}${colorePreferito}21</h1>`;
 else
      document.getElementById('psw').innerHTML = `
-     <h1>inserire un testo valido</h1>`
+     <h1>inserire un testo valido</h1>`;
